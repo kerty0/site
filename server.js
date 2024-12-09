37aac6969c422f70f02a33cc89ccbac092c638a6
kerty <g.kabakov@inbox.ru>
2024-12-09 18:43:10 +0300

    Cache

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-// const expressCache = require('cache-express')
-const favicon = require('express-favicon')
 const compression = require('compression')
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug')
-// app.enable('view cache')
-// app.use(express.json());
+app.use(express.json());
 app.use(compression())
+app.use((req, res, next) => {
+    if (req.url.startsWith('/static')) {
+        // res.set('Cache-Control', 'public, max-age=31536000');
+    }
+    next();
+})
 app.use('/static', express.static('static'))
 
 app.get('/', (req, res) => {
